11ece7761e043a2d3110e1f62eedb6e4332b0ce7
Hibiki Tsuboi <hibiki.tsuboi@gmail.com>
2026-02-09 16:57:57 +0900

    first commit

---
+++ Bankids/BankidsApp.swift
@@ -0,0 +1,32 @@
+//
+//  BankidsApp.swift
+//  Bankids
+//
+//  Created by Hibiki Tsuboi on 2026/02/09.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BankidsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Bankids/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bankids
+//
+//  Created by Hibiki Tsuboi on 2026/02/09.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
